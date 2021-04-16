e8439d6af5a99fb72744beec949f3ef16a659191
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-16 14:11:56 -0300

    Added proper index requests on streams page

--- twitch-2/client/src/components/streams/StreamList.jsx
+++ twitch-2/client/src/components/streams/StreamList.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => <div>StreamList</div>;
+class StreamList extends Component {
+  componentDidMount() {
+    const { fetchStreamsFunction } = this.props;
+    fetchStreamsFunction();
+  }
 
-export default StreamList;
+  render() {
+    return <div>StreamList</div>;
+  }
+}
+
+export default connect(null, { fetchStreamsFunction: fetchStreams })(StreamList);
