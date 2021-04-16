ee1aefcc85a1061066f593c303a5f66ef83f283e
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-16 14:34:20 -0300

    Implemented index streams page ui

--- twitch-2/client/src/components/streams/StreamList.jsx
+++ twitch-2/client/src/components/streams/StreamList.jsx
@@ -8,9 +8,33 @@ class StreamList extends Component {
     fetchStreamsFunction();
   }
 
+  renderList() {
+    const { streams } = this.props;
+    return streams.map((stream) => (
+      <div className="item" key={stream.id}>
+        <i className="large middle aligned icon camera" />
+        <div className="content">
+          {stream.title}
+          <div className="description">
+            {stream.description}
+          </div>
+        </div>
+      </div>
+    ));
+  }
+
   render() {
-    return <div>StreamList</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">
+          {this.renderList()}
+        </div>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchStreamsFunction: fetchStreams })(StreamList);
+const mapStateToProps = (state) => ({ streams: Object.values(state.streams) });
+
+export default connect(mapStateToProps, { fetchStreamsFunction: fetchStreams })(StreamList);
