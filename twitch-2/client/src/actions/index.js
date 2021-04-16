353a623f94746e4212a40f6ecd56965212e37aff
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-16 13:13:12 -0300

    Added actions for fetching stream, fetching, editing and deleting streams

--- twitch-2/client/src/actions/index.js
+++ twitch-2/client/src/actions/index.js
@@ -1,4 +1,12 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from './types';
 import streams from '../apis/streams';
 
 export const signIn = () => ({
@@ -11,5 +19,25 @@ export const signOut = () => ({
 
 export const createStream = (formValues) => async (dispatch) => {
   const res = await streams.post('/streams', formValues);
-  dispatch({ type: CREATE_STREAM, payload: res });
+  dispatch({ type: CREATE_STREAM, payload: res.data });
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const res = await streams.get('/streams');
+  dispatch({ type: FETCH_STREAMS, payload: res.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const res = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: res.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const res = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: res.data });
 };
