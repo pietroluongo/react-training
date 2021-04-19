e846dfe0e4fe65ad6b235db24b73168cc09038d6
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-19 09:19:17 -0300

    Added styling to modal, modal is now dismissable

--- twitch-2/client/src/components/Modal.jsx
+++ twitch-2/client/src/components/Modal.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
 
 const Modal = (props) => ReactDOM.createPortal(
-  <div className="ui dimmer modals visible active">
-    <div className="ui standard modal visible active">
-      modalContent
+  <div role="none" onClick={() => history.push('/')} className="ui dimmer modals visible active">
+    <div role="none" onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+      <div className="header">Delete Stream</div>
+      <div className="content">
+        Are you sure you want to delete the stream?
+      </div>
+      <div className="actions">
+        <button className="ui primary button" type="button">Delete</button>
+        <button className="ui button" type="button">Cancel</button>
+      </div>
     </div>
   </div>,
   document.querySelector('#modal'),
