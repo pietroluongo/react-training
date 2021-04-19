8bf14f51f38c33ed4b1265b157fc18c51ca49bdf
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-19 09:53:33 -0300

    Wired StreamDelete to properly request stream data

--- twitch-2/client/src/components/streams/StreamDelete.jsx
+++ twitch-2/client/src/components/streams/StreamDelete.jsx
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Modal from '../Modal';
 import history from '../../history';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-  const actions = (
-    <>
-      <div className="ui button negative">
-        Delete
-      </div>
-      <div className="ui button">
-        Cancel
-      </div>
-    </>
-  );
+class StreamDelete extends Component {
+  componentDidMount() {
+    const { match, fetchStreamFunction } = this.props;
+    fetchStreamFunction(match.params.id);
+  }
 
-  return (
-    <div>
+  renderActions() {
+    return (
+      <>
+        <div className="ui button negative">
+          Delete
+        </div>
+        <div className="ui button">
+          Cancel
+        </div>
+      </>
+    );
+  }
+
+  renderContent() {
+    const { stream } = this.props;
+    if (!stream) {
+      return 'Are you sure you want to delete this stream?';
+    }
+    return `Are you sure you want to delete the stream "${stream.title}"?`;
+  }
+
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Are you sure you want to delete the stream?"
-        actions={actions}
+        content={this.renderContent()}
+        actions={this.renderActions()}
         onDismiss={() => history.push('/')}
       />
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => ({ stream: state.streams[ownProps.match.params.id] });
+
+export default connect(mapStateToProps, { fetchStreamFunction: fetchStream })(StreamDelete);
