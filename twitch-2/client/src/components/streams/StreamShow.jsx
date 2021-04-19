1f3b7500d5b1ed03e08670aa50d13125638b2a99
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-19 10:24:27 -0300

    StreamShow now properly fetches the needed data

--- twitch-2/client/src/components/streams/StreamShow.jsx
+++ twitch-2/client/src/components/streams/StreamShow.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => <div>StreamShow</div>;
+class StreamShow extends Component {
+  componentDidMount() {
+    const { fetchStreamFunc, match } = this.props;
+    fetchStreamFunc(match.params.id);
+  }
 
-export default StreamShow;
+  render() {
+    const { stream } = this.props;
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h1>{stream.title}</h1>
+        <h5>{stream.description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({ stream: state.streams[ownProps.match.params.id] });
+
+export default connect(mapStateToProps, { fetchStreamFunc: fetchStream })(StreamShow);
