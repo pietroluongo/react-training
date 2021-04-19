63b9b969a3514517fde79ecc754e0348d33a3ce6
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-19 07:20:22 -0300

    Added StreamForm component

---
+++ twitch-2/client/src/components/streams/StreamForm.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamForm extends Component {
+  onSubmit = (formValues) => {
+    const { onSubmit } = this.props;
+    onSubmit(formValues);
+  }
+
+  renderError({ error, touched }) {
+    if (error && touched) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+    return null;
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label htmlFor={input.name}>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div>
+        <form onSubmit={handleSubmit(this.onSubmit)} className="ui form error">
+          <Field name="title" component={this.renderInput} label="Enter Title" />
+          <Field name="description" component={this.renderInput} label="Enter Description" />
+          <button type="submit" className="ui button primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'A title is needed.';
+  }
+  if (!formValues.description) {
+    errors.description = 'A description is needed.';
+  }
+  return errors;
+};
+
+export default reduxForm({
+  form: 'streamForm',
+  validate,
+})(StreamForm);
