3be24f8716da4216f5a416bf3e5bb89f2d58a60e
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-19 09:28:17 -0300

    Updated Modal to receive props properly

--- twitch-2/client/src/components/Modal.jsx
+++ twitch-2/client/src/components/Modal.jsx
@@ -5,13 +5,12 @@ import history from '../history';
 const Modal = (props) => ReactDOM.createPortal(
   <div role="none" onClick={() => history.push('/')} className="ui dimmer modals visible active">
     <div role="none" onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
-      <div className="header">Delete Stream</div>
+      <div className="header">{props.title}</div>
       <div className="content">
-        Are you sure you want to delete the stream?
+        {props.content}
       </div>
       <div className="actions">
-        <button className="ui primary button" type="button">Delete</button>
-        <button className="ui button" type="button">Cancel</button>
+        {props.actions}
       </div>
     </div>
   </div>,
--- twitch-2/client/src/components/streams/StreamDelete.jsx
+++ twitch-2/client/src/components/streams/StreamDelete.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import Modal from '../Modal';
 
-const StreamDelete = () => <div>StreamDelete<Modal /></div>;
+const StreamDelete = () => {
+  const actions = (
+    <div>
+      <div className="ui button negative">
+        Delete
+      </div>
+      <div className="ui button">
+        Cancel
+      </div>
+    </div>
+  );
+
+  return (
+    <div>
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete the stream?"
+        actions={actions}
+      />
+    </div>
+  );
+};
 
 export default StreamDelete;
