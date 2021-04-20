b5ab1961c370622079ca5f6e3ee262532f613066
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-20 10:46:33 -0300

    Implemented LanguageContext component

--- translate/src/contexts/LanguageContext.js
+++ translate/src/contexts/LanguageContext.js
@@ -1,3 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default React.createContext('en');
+const Context = React.createContext('en');
+
+export class LanguageStore extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { language: 'en' };
+  }
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+        {children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
