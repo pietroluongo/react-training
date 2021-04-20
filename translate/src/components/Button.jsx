f5a3a5864e807622bf93f5474605d440d70a8637
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-20 09:41:25 -0300

    Associated Button and Field with LanguageContext, and implemented their translations

--- translate/src/components/Button.jsx
+++ translate/src/components/Button.jsx
@@ -4,9 +4,20 @@ import LanguageContext from '../contexts/LanguageContext';
 class Button extends Component {
   static contextType = LanguageContext;
 
+  getButtonText() {
+    switch (this.context) {
+      case 'nl':
+        return 'Voorleggen';
+      case 'br':
+        return 'Enviar';
+      default:
+        return 'Submit';
+    }
+  }
+
   render() {
     console.log(this.context);
-    return <button type="submit" className="ui button primary">Submit</button>;
+    return <button type="submit" className="ui button primary">{this.getButtonText()}</button>;
   }
 }
 
--- translate/src/components/Field.jsx
+++ translate/src/components/Field.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends Component {
-  render() {
-    return (
-      <div className="ui field">
-        <label htmlFor="name">Name</label>
-        <input name="name" />
-      </div>
-    );
-  }
+    static contextType = LanguageContext;
+
+    getLabelText() {
+      switch (this.context) {
+        case 'br':
+          return 'Nome';
+        case 'nl':
+          return 'Naam';
+        default:
+          return 'Name';
+      }
+    }
+
+    render() {
+      return (
+        <div className="ui field">
+          <label htmlFor="name">{this.getLabelText()}</label>
+          <input name="name" />
+        </div>
+      );
+    }
 }
 
 export default Field;
