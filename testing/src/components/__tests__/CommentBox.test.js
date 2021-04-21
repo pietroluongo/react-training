7dd00116d05da2909d45c61792a75dc6bf506503
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-21 14:43:14 -0300

    Added test suite to commentbox component

---
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has a text area and a button', () => {
+  expect(wrapped.find('textarea')).toHaveLength(1);
+  expect(wrapped.find('button')).toHaveLength(1);
+});
