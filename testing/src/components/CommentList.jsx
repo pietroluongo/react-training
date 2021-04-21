b472cecd673edcae84a82c2be869bc602ad1ac84
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-21 16:36:21 -0300

    Implemented actual comment list functionality

--- testing/src/components/CommentList.jsx
+++ testing/src/components/CommentList.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const CommentList = () => <div>CommentList</div>;
+class CommentList extends Component {
+  renderComments() {
+    const { comments } = this.props;
+    return comments.map((comment) => <li key={comment}>{comment}</li>);
+  }
 
-export default CommentList;
+  render() {
+    return <div>{this.renderComments()}</div>;
+  }
+}
+
+const mapStateToProps = (state) => ({ comments: state.comments });
+
+export default connect(mapStateToProps, {})(CommentList);
