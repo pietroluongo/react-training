81d3cd2c8dfe17896a8e2538fcdff623388bf233
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-21 16:27:20 -0300

    Added save comment action test

---
+++ testing/src/actions/__tests__/actions.test.js
@@ -0,0 +1,14 @@
+import { saveComment } from 'actions';
+import { SAVE_COMMENT } from 'actions/types';
+
+describe('saveComment', () => {
+  it('has the correct type', () => {
+    const action = saveComment();
+    expect(action.type).toEqual(SAVE_COMMENT);
+  });
+
+  it('has the correct payload', () => {
+    const action = saveComment('New dummy comment');
+    expect(action.payload).toEqual('New dummy comment');
+  });
+});
