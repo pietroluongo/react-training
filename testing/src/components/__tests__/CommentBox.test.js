c23f95bc7545dfaaff13c748ad4a14a2df2441db
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-21 15:21:57 -0300

    Added commentBox test to verify if the textarea can be manipulated by the user

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,12 @@ it('has a text area and a button', () => {
   expect(wrapped.find('textarea')).toHaveLength(1);
   expect(wrapped.find('button')).toHaveLength(1);
 });
+
+it('has a text area that users can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' },
+  });
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
