bf4ebe24abef91635bb50c369094d42564a7f74b
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-21 17:12:24 -0300

    Added base for remote comment fetching

--- testing/src/Root.jsx
+++ testing/src/Root.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
+import reduxPromise from 'redux-promise';
 
 export default ({ children, initialState = {} }) => (
-  <Provider store={createStore(reducers, initialState)}>
+  <Provider
+    store={createStore(
+      reducers,
+      initialState,
+      applyMiddleware(reduxPromise),
+    )}
+  >
     {children}
   </Provider>
 );
