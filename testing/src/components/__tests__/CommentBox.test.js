9f6c4f5f0bf5ad95aa7a4f88ba666023e8c03ee8
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-21 17:29:22 -0300

    Fixed broken commentBox test

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -15,7 +15,7 @@ afterEach(() => {
 
 it('has a text area and a button', () => {
   expect(wrapped.find('textarea')).toHaveLength(1);
-  expect(wrapped.find('button')).toHaveLength(1);
+  expect(wrapped.find('button')).toHaveLength(2);
 });
 
 describe('the text area', () => {
