23d199e436d3de96deed92e3358e4c6bf7a12149
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-21 14:05:15 -0300

    Refactored test suite to use beforeEach jest command

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import App from '../App';
+import App from 'components/App';
 import { shallow } from 'enzyme';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
-it('shows a comment box',() => {
-    const wrapped = shallow(<App />);
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
+
+it('shows a comment box', () => {
     expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', () => {
-    const wrapped = shallow(<App />);
     expect(wrapped.find(CommentList).length).toEqual(1);
 });
