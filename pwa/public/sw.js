a936d7bbc18100afb6edcd4c4856623e59a91d62
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-22 12:02:37 -0300

    Added fetch event listener

--- pwa/public/sw.js
+++ pwa/public/sw.js
@@ -6,3 +6,7 @@ self.addEventListener('activate', (ev) => {
   console.log('[Service Worker] Activating Service Worker...', ev);
   return self.clients.claim();
 });
+
+self.addEventListener('fetch', (ev) => {
+  console.log('[Service Worker] Fetching...', ev);
+});
