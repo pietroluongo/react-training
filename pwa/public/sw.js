e33a37b6350ec2dda8b1b1e2465696fdd6a0449a
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-23 09:27:26 -0300

    Added caching to allow app to work offline

--- pwa/public/sw.js
+++ pwa/public/sw.js
@@ -1,5 +1,22 @@
 self.addEventListener('install', (ev) => {
   console.log('[Service Worker] Installing Service Worker...', ev);
+  ev.waitUntil(
+    caches.open('static').then((cache) => {
+      console.log('[Service Worker] Precaching App Shell');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+      ]);
+    }, () => {
+      console.log('[Service Worker] Failed to Precache App');
+    }),
+  );
 });
 
 self.addEventListener('activate', (ev) => {
@@ -8,5 +25,12 @@ self.addEventListener('activate', (ev) => {
 });
 
 self.addEventListener('fetch', (ev) => {
-  console.log('[Service Worker] Fetching...', ev);
+  ev.respondWith(
+    caches.match(ev.request).then((res) => {
+      if (res) {
+        return res;
+      }
+      return fetch(ev.request);
+    }),
+  );
 });
